9c1ee38a7f61dc63021bbbcb5eba2a865f0a6e1b
Eric <ejzagala@gmail.com>
2018-07-04 15:02:28 -0600

    yarn locks (re)moved

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,9 @@ const apiRoutes = require("./api");
 router.use("/api", apiRoutes);
 
 // If no API routes are hit, send the React app
+// This is pointing to pubilc right now, I think it may need to point to build to deploy successfully
 router.use(function(req, res) {
-  res.sendFile(path.join(__dirname, "../client/public/index.html"));
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 module.exports = router;
